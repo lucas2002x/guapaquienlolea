8d757b75e0471127673f957ccb15efed95c6b257
lucas2002x <100lucaschavez100@gmail.com>
2024-12-18 20:33:56 +0100

    Add files via upload

---
+++ scritp.js
@@ -0,0 +1,10 @@
+yesBtn.addEventListener("mouseover",function(){
+    const randomX = parseInt(Math.random()*1000);
+    const randomY = parseInt(Math.random()*1000);
+    yesBtn.style.setProperty("top",randomY+"%");
+    yesBtn.style.setProperty("left",randomX+"%");
+    yesBtn.style.setProperty("transform",`translate(-${randomX}%,-${randomY}%)`);
+})
+noBtn.addEventListener("click",function(){
+    alert("Creo que eres increíble y me encantaría conocerte mejor ¿Podemos salir a tomar algo este fin de semana?");
+})
