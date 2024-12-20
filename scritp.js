9af762d76a0fd00cb9712b4eb313e3a52d68cf83
lucas2002x <100lucaschavez100@gmail.com>
2024-12-20 01:00:55 +0100

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -6,5 +6,5 @@ yesBtn.addEventListener("mouseover",function(){
     yesBtn.style.setProperty("transform",`translate(-${randomX}%,-${randomY}%)`);
 })
 noBtn.addEventListener("click",function(){
-    alert("He estado pensando mucho si enviarte esto o no, pero creo que lo mejor es ser honesto,creo que eres increíble y me encantaría conocerte mejor ¿Podemos salir a tomar algo este fin de semana?");
+    alert("He estado pensando mucho si enviarte esto o no, he lleagado a la conclusión de que lo mejor es ser honesto, creo que eres increíble y me encantaría conocerte mejor ¿Podemos salir a tomar algo este fin de semana?");
 })
